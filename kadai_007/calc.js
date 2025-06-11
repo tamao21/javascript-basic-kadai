adcf31742c70456ec56970a36f6c01fb8cf4f9ed
tamao21 <tt21komasan@icloud.com>
2025-06-11 23:12:35 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,21 @@
+let num = 10;
+if (num % 3 == 0) {
+  console.log("3の倍数です");
+
+
+}
+else if (num % 5 == 0) {
+  console.log("5の倍数です");
+
+
+}
+else if (num % 5 == 0 && num % 5 == 0) {
+  console.console.log("3と5の倍数です");
+
+
+}
+else {
+  console.log(num);
+
+
+}

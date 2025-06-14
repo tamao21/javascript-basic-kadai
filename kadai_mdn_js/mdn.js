f0614fd9597e0be815851d99f06b5d05abc1ff31
tamao21 <tt21komasan@icloud.com>
2025-06-14 20:19:17 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
 const today = new Date();
 const year = today.getFullYear();
-const month = today.getMonth();
-const data = today.getDate();
+const month = today.getMonth()+1;
+const day = today.getDate();
 // 出力
-console.log(year + '年' + month + '月' + data + '日');
+console.log(year + '年' + month + '月' + day + '日');

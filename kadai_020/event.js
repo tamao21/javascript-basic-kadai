f42bd8cec650c6473c652e5ddf474341f49412e8
tamao21 <tt21komasan@icloud.com>
2025-06-21 22:58:49 +0900

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,6 +1,6 @@
-const click_btn = document.getElementById("text");
+const click_btn = document.getElementById("btn");
 const text = document.getElementById("text");
 
 click_btn.addEventListener('click', () => {
-  text.textContent = (ボタンをクリックしました);
+  text.textContent = "ボタンをクリックしました";
 });
